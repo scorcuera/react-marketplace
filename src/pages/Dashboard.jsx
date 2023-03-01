91e5966ccf68ac42de423e5003ca143ed1697463
scorcuera <scorcuera6@gmail.com>
2023-03-01 22:08:11 +0100

    added filter feature

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,41 @@
 import { useLoaderData } from "react-router-dom"
-import { useState } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { productHandler } from "../handlers/productHandler";
 import Product from '../components/Product'
 import './Dashboard.css'
 
 function Dashboard() {
-    const { products } = useLoaderData();
-    const [productsData, setProductsData] = useState(products);
+  const { products } = useLoaderData();
+  const [productsData, setProductsData] = useState(products);
+  const [searchValue, setSearchValue] = useState("");
 
-    const deleteProduct = async (id) => {
-      await productHandler.deleteProduct(id);
-      setProductsData(productsData.filter(product => product.id !== id))
+  const deleteProduct = async (id) => {
+    await productHandler.deleteProduct(id);
+    setProductsData(productsData.filter(product => product.id !== id));
   }
 
-    return (
-        <>
-        {productsData.map((product) => (
-            <Product key={product.id} product={product} deleteProduct={deleteProduct} />
+  const handleSearch = (event) => {
+    let searchInput = event.target.value;
+    setSearchValue(searchInput);
+  }
+
+  let filteredProducts = productsData.filter(product => {
+    return product.title.toLowerCase().includes(searchValue.toLowerCase());
+  })
+
+  return (
+    <div className="block--dashboard">
+      <div className="block--search-container">
+        <label htmlFor="">Search</label>
+        <input type="input" onChange={handleSearch} />
+      </div>
+      <section className="block--products-container">
+        {filteredProducts.map((product) => (
+          <Product key={product.id} product={product} deleteProduct={deleteProduct} />
         ))}
-      </>
-    )
+      </section>
+    </div>
+  )
 }
 
 export default Dashboard;
